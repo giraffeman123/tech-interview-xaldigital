c3cca02cdbf526fd777c1be405e1d561d4d656bf
eduardo elliot martinez palomino <elliotmtz12@gmail.com>
2024-10-23 10:51:02 -0700

    added liveness probe to api

--- api/index.js
+++ api/index.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 8880;
 app.use(express.json());
 app.use(`/flights`, flightRouter);
 app.use(`/answers`, answerRouter);
+app.use(`/liveness`, (req, res, next) => {
+	res.status(200).json('fsa API is alive');
+});
+
 
 app.use((req, res, next) => {
     res.status(404).json(
